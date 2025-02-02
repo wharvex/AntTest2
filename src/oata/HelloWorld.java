b5e0fc36b6f81c831a83528115ac9b97d6bf1267
Tim <81175852+wharvex@users.noreply.github.com>
2025-02-02 18:18:31 -0500

    get junitreport task working

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -8,4 +8,11 @@ public class HelloWorld {
         logger.info("Log Hello World");
         System.out.println("Hello World");
     }
+
+    public static int evaluate(String expression) {
+       int sum = 0;
+       for (String summand: expression.split("\\+"))
+         sum += Integer.valueOf(summand);
+       return sum;
+    }
 }
--- src/oata/HelloWorldTest.java
+++ src/oata/HelloWorldTest.java
@@ -4,15 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.fail;
 
-public class HelloWorldTest {
+import static org.junit.Assert.assertEquals;
 
+public class HelloWorldTest {
     @Test
-    public void testNothing() {
+    public void evaluatesExpression() {
+        int sum = HelloWorld.evaluate("1+2+3");
+        assertEquals(6, sum);
     }
 
     @Test
     public void testWillAlwaysFail() {
-        fail("An error message");
+        assertEquals(1, 2);
     }
-
 }
